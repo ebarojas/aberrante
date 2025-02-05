315331fc3ddd28fc0f62db5df6846994ba292028
Everardo J Barojas M <everardo@prescrypto.com>
2025-02-05 17:19:54 -0600

    Improve depoloy_migrate script

--- bin/deploy_migrate.sh
+++ bin/deploy_migrate.sh
@@ -4,6 +4,6 @@
 
 heroku maintenance:on --app aberrante
 # heroku scale worker=0 --app aberrante
-heroku run python ./aberrante/manage.py migrate --app aberrante
+heroku run python ./manage.py migrate --app aberrante
 # heroku scale worker=1 --app aberrante
 heroku maintenance:off --app aberrante
