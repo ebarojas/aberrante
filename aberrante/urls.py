484729fc79e9f2fdcb9b50e2874ec3d4de14f4d8
Everardo J Barojas M <everardo@prescrypto.com>
2025-02-06 15:16:38 -0600

    Add sitemap and robots for SEO performance

--- aberrante/urls.py
+++ aberrante/urls.py
@@ -15,14 +15,33 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from .views import home
 from django.conf import settings
 from django.conf.urls.static import static
+# For robots.txt
+from django.views.generic.base import RedirectView
+from django.http import HttpResponse
+# TemplateView for Sitemap
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'), # home minimal
+    re_path(
+        r"^robots.txt$",
+        lambda r: HttpResponse(
+            "User-agent: *\nDisallow: /admin/\nSitemap: https://www.aberrante.art/sitemap.xml",
+            content_type="text/plain",
+        ),
+    ),
+    # Sitemap
+    re_path(
+        r"^sitemap.xml$",
+        TemplateView.as_view(
+            template_name="sitemap.xml", content_type="application/xml; charset=utf-8"
+        ),
+    ),
 ]
 
 # Serve media files in development only
