99ee391211eaf90baa6fb4fb2920af16edfd2bbb
Everardo J Barojas M <everardo@prescrypto.com>
2025-02-08 15:36:04 -0600

    Customise admin panel

--- aberrante/admin.py
+++ aberrante/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Lead  # Import your custom model
 
-admin.site.register(Lead)  # Register it in the admin panel
+
+admin.site.site_header = "Aberrante Admin Panel"  # Top-left header
+admin.site.site_title = "Aberrante"  # Browser tab title
+admin.site.index_title = "Bienvenido a Aberrante"  # Dashboard title
+
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "type", "created_at") 
+    readonly_fields = ("id", "created_at")
+
+admin.site.register(Lead, LeadAdmin)
